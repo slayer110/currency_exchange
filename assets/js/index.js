1a5e130d4156f407de387f283d32d8bd838bb1ef
Viktar Hlushchenia <slayer110@yandex.ru>
2019-09-15 14:46:38 +0300

    currency

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
 //получение элементов страницы и объявление переменных
+const editRateSale = document.getElementById('sale');
+const editRatePurchase = document.getElementById('purchase');
+const editCur = document.getElementById('cur');
+const buttonSaveEdit=document.querySelectorAll('button.btn.btn-primary');
 // let saveQuery;
 // const saveButton = document.querySelector('input[type=submit]');
 // const sale = document.getElementById('sale');
@@ -40,5 +44,28 @@
 //     body: JSON.stringify({saleVal: sale.value, purchaseVal: purchase.value, currencyVal: selectCur.value})
 //   })
 // });
-const tableOfCurrency=document.getElementsByClassName('table');
+const tableOfCurrency = document.getElementsByClassName('table')[0].getElementsByTagName('tbody')[0];
+const listCur = fetch('http://exchange.vi/assets/php/rateExchange.phtml').then(result => result.json())
+  .then(rates => rates.forEach(elem => {
+    const tr = document.createElement('tr');
+    for (let i in elem) {
+      if (i === 'currency_id') {
+        tr.setAttribute('curId', elem[i]);
+        continue
+      }
+      const td = document.createElement('td');
+      td.innerHTML = elem[i];
+      tr.appendChild(td);
+    }
+    const td = document.createElement('td');
+    td.innerHTML = '<i class="fas fa-pen" style="color: blue;cursor: pointer" data-toggle="modal" data-target="#exampleModal"></i>';
+    td.addEventListener('click', (e) => {
+      const curId = e.target.parentElement.parentElement;
+      editCur.innerText = curId.cells[1].innerText;
+      editRateSale.value = curId.cells[2].innerHTML;
+      editRatePurchase.value = curId.cells[3].innerHTML;
+    });
+    tr.appendChild(td);
+    tableOfCurrency.appendChild(tr)
+  }));
 
