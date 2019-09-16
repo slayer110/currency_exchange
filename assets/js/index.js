383ad00be319cdb13e8ba426533e9d215de15d57
Viktar Hlushchenia <slayer110@yandex.ru>
2019-09-16 11:20:53 +0300

    currency

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,25 @@
 //получение элементов страницы и объявление переменных
-const editRateSale = document.getElementById('sale');
-const editRatePurchase = document.getElementById('purchase');
-const editCur = document.getElementById('cur');
-const buttonSaveEdit = document.querySelectorAll('button.btn.btn-secondary')[0];
-let saveQuery;
-// const saveButton = document.querySelector('input[type=submit]');
-// const sale = document.getElementById('sale');
-// const purchase = document.getElementById('purchase');
-// const selectCur = document.getElementById('currency');
-
+const selectCur = document.getElementById('currency');
+const rate = document.querySelector('#rate');
 //Подключение списка валют в выпадающий список
-// const listCur = fetch('http://exchange.vi/assets/php/listOfCurrency.phtml').then(res => res.json())
-//   .then(list => {
-//     list.forEach((elem) => {
-//       selectCur.appendChild(new Option(elem['code'], elem['id']))
-//     });
-//     const definitionSelectedElement = () => {
-//       for (let i = 0; i < selectCur.options.length; i++) {
-//         if (selectCur.options[i].selected) {
-//           // alert(selectCur.options[i].value)
-//         }
-//       }
-//     };
-//     definitionSelectedElement();
-//     selectCur.addEventListener('change', () => {
-//       definitionSelectedElement();
-//     })
-//   });
-
-//сохранение курса валюты
-const tableOfCurrency = document.getElementsByClassName('table')[0].getElementsByTagName('tbody')[0];
-updateListRates = () => {
-  fetch('http://exchange.vi/assets/php/rateExchange.phtml').then(result => result.json())
-    .then(rates => {
-      tableOfCurrency.innerHTML = "";
-      rates.forEach(elem => {
-        const tr = document.createElement('tr');
-        for (let i in elem) {
-          if (i === 'currency_id') {
-            tr.setAttribute('curId', elem[i]);
-            continue
-          }
-          const td = document.createElement('td');
-          td.innerHTML = elem[i];
-          tr.appendChild(td);
-        }
-        const td = document.createElement('td');
-        td.innerHTML = '<i class="fas fa-pen" style="color: blue;cursor: pointer" data-toggle="modal" data-target="#exampleModal"></i>';
-        td.addEventListener('click', (e) => {
-          const curId = e.target.parentElement.parentElement;
-          editCur.innerText = curId.cells[1].innerText;
-          editRateSale.value = curId.cells[2].innerHTML;
-          editRatePurchase.value = curId.cells[3].innerHTML;
-          editRatePurchase.setAttribute('curId', curId.getAttribute('curId'))
-        });
-        tr.appendChild(td);
-        tableOfCurrency.appendChild(tr)
-      });
+fetch('http://exchange.vi/assets/php/rateExchange.phtml').then(res => res.json())
+  .then(list => {
+    console.log(list);
+    list.forEach((elem) => {
+      selectCur.appendChild(new Option(elem['code'], elem['currency_id']))
     });
-}
-
-updateListRates();
-//сохранение изменений
-buttonSaveEdit.addEventListener('click', (e) => {
-  if (saveQuery) {
-    saveQuery = undefined
-  }
-  e.preventDefault();
-  saveQuery = fetch('http://exchange.vi/assets/php/saveCurrency.phtml', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      saleVal: editRateSale.value,
-      purchaseVal: editRatePurchase.value,
-      currencyVal: editRatePurchase.getAttribute('curId'),
+    const definitionSelectedElement = () => {
+      for (let i = 0; i < selectCur.options.length; i++) {
+        if (selectCur.options[i].selected) {
+         let row= list.filter((elem) => {
+            return parseInt(elem['currency_id'])=== parseInt(selectCur.options[i].value)
+          });
+         rate.innerText=row[0]['sale'];
+        }
+      }
+    };
+    definitionSelectedElement();
+    selectCur.addEventListener('change', () => {
+      definitionSelectedElement();
     })
-  })
-  updateListRates();
-});
+  });
